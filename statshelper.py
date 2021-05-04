8adf6bd9632cd1597a1888b6742e95125b4c6710
alexandermichels <michels9@illinois.edu>
2021-05-04 17:16:49 -0500

    major update

--- statshelper.py
+++ statshelper.py
@@ -1,5 +1,7 @@
+import geopandas as gpd
+from matplotlib import cm, colors
 import matplotlib.pyplot as plt
-from mgwr.utils import shift_colormap,truncate_colormap
+from mgwr.utils import shift_colormap, truncate_colormap
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
@@ -9,11 +11,11 @@ def gwr_param_plots(result, gdf, names=[], filter_t=False, subplots=(2,2), figsi
     #Size of the plot. Here we have a 2 by 2 layout.
     fig, axs = plt.subplots(subplots[0], subplots[1], figsize=figsize)
     axs = axs.ravel()
-    k = gwr_results.k
+    k = result.k
     #The max and min values of the color bar.
     vmin = -0.8
     vmax = 0.8
-    cmap = cm.get_cmap("coolwarm")
+    cmap = cm.get_cmap("viridis")
     norm = colors.BoundaryNorm(np.arange(-0.8,0.9,0.1),ncolors=256)
     if (vmin < 0) & (vmax < 0):
         cmap = truncate_colormap(cmap, 0.0, 0.5)
@@ -23,12 +25,12 @@ def gwr_param_plots(result, gdf, names=[], filter_t=False, subplots=(2,2), figsi
         cmap = shift_colormap(cmap, start=0.0, midpoint=1 - vmax/(vmax + abs(vmin)), stop=1.)    
     for j in range(k): 
         pd.concat([gdf,pd.DataFrame(np.hstack([result.params,result.bse]))],axis=1).plot(ax=axs[j],column=j,vmin=vmin,vmax=vmax,
-                   cmap="bwr",norm=norm,linewidth=0.1,edgecolor='white')
+                   cmap=cmap,norm=norm,linewidth=0.1,edgecolor='white')
         axs[j].set_title("Parameter estimates of \n" + names[j],fontsize=10)
         if filter_t:
             rslt_filtered_t = result.filter_tvals()
             if (rslt_filtered_t[:,j] == 0).any():
-                gdf[rslt_filtered_t[:,j] == 0].plot(color='lightgrey', ax=axs[j],linewidth=0.1,edgecolor='white')
+                gdf[rslt_filtered_t[:,j] == 0].plot(color='lightgrey', ax=axs[j],linewidth=0.1,edgecolor='white', alpha=0.2)
         #plt.axis('off')
     fig = axs[j].get_figure()
     cax = fig.add_axes([0.99, 0.2, 0.025, 0.6])
